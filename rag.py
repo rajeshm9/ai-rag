df0d4ae504b87070b7af63088a618cc845fee492
rajeshm9 <rajeshmahajan09@gmail.com>
2025-08-10 13:12:58 +0530

    Changed Model to Ollama

--- rag.py
+++ rag.py
@@ -16,17 +16,22 @@ from typing_extensions import List, TypedDict
 
 #llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 #embeddings = DeterministicFakeEmbedding(size=4096)
+print("Loading LLM and embeddings...")
+#curl -fsSL https://ollama.com/install.sh | sh
+#ollama pull llama3.1
 
-llm = ChatOllama(model="mistral", temperature=0)
+llm = ChatOllama(model="llama3.1", temperature=0)
 # Use Ollama for embeddings
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+embeddings = OllamaEmbeddings(model="llama3.1")
 
 vector_store = InMemoryVectorStore(embeddings)
+
+print("Loading HTML documents...")
 
 html_docs = load_html_docs()
 
 print(f"Loaded {len(html_docs)} HTML documents.")
-print(html_docs[0].page_content[:200])  # Print first 200 characters of the first document
+#print(html_docs[0].page_content[:200])  # Print first 200 characters of the first document
 
 
 document_chunking = document_chunking(html_docs)
@@ -65,6 +70,23 @@ graph = graph_builder.compile()
 
 print(graph)
 # Test the application
+
+while True:
+    print("Say 'exit' or 'quit' to exit the loop")
+    question = input('User question: ')
+    print(f"Question: {question}")
+    if question.lower() in ["exit", "quit"]:
+        print("Exiting the conversation. Goodbye!")
+        break
+    response = graph.invoke({"question": question})
+    print(response["answer"])
+    #formatted_prompt = prompt.format(context=retrieved_context, question=question)
+    #response_from_model = model.invoke(formatted_prompt)
+    #parsed_response = parser.parse(response_from_model)
+    #print(f"Answer: {parsed_response}")
+    #print()
+
+
 response = graph.invoke({"question": "What is 5G?"})
 print(response["answer"])
 
