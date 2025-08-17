97a755adb5cbaff891fbc1e44cf7eec589664fce
rajeshm9 <rajeshmahajan09@gmail.com>
2025-08-17 13:01:41 +0530

    smolagents

--- rag.py
+++ rag.py
@@ -1,18 +1,19 @@
 from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
-
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from html_loader import *
 from document_chunk import document_chunking
+
+from langchain_core.documents import Document
+from langchain import hub
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+import os
 
 from langchain.chat_models import init_chat_model
 from langchain_core.embeddings import DeterministicFakeEmbedding
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_core.documents import Document
-
-from langchain import hub
-from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
 
 #llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 #embeddings = DeterministicFakeEmbedding(size=4096)
@@ -24,21 +25,30 @@ llm = ChatOllama(model="llama3.1", temperature=0)
 # Use Ollama for embeddings
 embeddings = OllamaEmbeddings(model="llama3.1")
 
-vector_store = InMemoryVectorStore(embeddings)
 
-print("Loading HTML documents...")
-
-html_docs = load_html_docs()
-
-print(f"Loaded {len(html_docs)} HTML documents.")
-#print(html_docs[0].page_content[:200])  # Print first 200 characters of the first document
-
-
-document_chunking = document_chunking(html_docs)
-print(f"Split blog post into {len(document_chunking)} sub-documents.")
 
 # Index chunks
-_ = vector_store.add_documents(documents=document_chunking)
+PERSIST_DIR = "./vector_store"
+
+if not os.path.exists(PERSIST_DIR):
+    os.makedirs(PERSIST_DIR)
+    # Load HTML docs
+    html_docs = load_html_docs()
+    print(f"Loaded {len(html_docs)} HTML documents.")
+    print(html_docs[0].page_content[:200])
+    
+    # Chunk documents
+    document_chunks = document_chunking(html_docs)
+    print(f"Split blog post into {len(document_chunks)} sub-documents.")
+    
+    # Create Chroma DB and persist
+    vector_store = Chroma.from_documents(documents=document_chunks, embedding=embeddings, persist_directory=PERSIST_DIR)
+    vector_store.persist()
+else:
+    vector_store = Chroma(persist_directory=PERSIST_DIR, embedding_function=embeddings)
+
+
+# 4. Load prompt from LangChain hub
 
 prompt = hub.pull("rlm/rag-prompt")
 
@@ -80,13 +90,6 @@ while True:
         break
     response = graph.invoke({"question": question})
     print(response["answer"])
-    #formatted_prompt = prompt.format(context=retrieved_context, question=question)
-    #response_from_model = model.invoke(formatted_prompt)
-    #parsed_response = parser.parse(response_from_model)
-    #print(f"Answer: {parsed_response}")
-    #print()
 
 
-response = graph.invoke({"question": "What is 5G?"})
-print(response["answer"])
 
